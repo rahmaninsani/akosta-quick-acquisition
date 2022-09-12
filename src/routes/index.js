10a85172f4d514bd823e55407b803a3a69acaf9f
Rahman Insani <rahmaninsani54@gmail.com>
2022-09-12 15:59:47 +0700

    Adding Auth Middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
+const { isAuthenticated } = require('../middlewares');
 
 const loginRoute = require('./login.route');
 const dashboardRoute = require('./dashboard.route');
 const hostelRoute = require('./hostel.route');
 
 router.use('/login', loginRoute);
-router.use('/dashboard', dashboardRoute);
-router.use('/hostel', hostelRoute);
+router.use('/dashboard', isAuthenticated, dashboardRoute);
+router.use('/hostel', isAuthenticated, hostelRoute);
 router.use('/', (req, res) => {
   res.redirect('/login');
 });
