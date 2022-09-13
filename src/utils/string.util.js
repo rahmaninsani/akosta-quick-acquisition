f3fb15782f5b43e32dae748b4f558b3748db725f
Rahman Insani <rahmaninsani54@gmail.com>
2022-09-14 01:18:49 +0700

    Update string.util.js

--- src/utils/string.util.js
+++ src/utils/string.util.js
@@ -1,7 +1,6 @@
 const toCapitalizeEachWord = (text) => {
   const words = text.split(' ');
   const capitalizedWords = words.map((word) => {
-    console.log(word);
     return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
   });
 
